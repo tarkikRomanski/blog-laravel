2424cc109f875afba28812c84e6f878477a854b9
tarkikRomanski <tarkik@ukr.net>
2018-07-21 17:40:10 +0300

    added view cascade

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <categories></categories>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>My blog</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="app">
+    <nav class="navbar navbar-findcond">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="{{ route('categories') }}">My blog</a>
+            </div>
+        </div>
+    </nav>
+    @yield('content')
+</div>
+<script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
