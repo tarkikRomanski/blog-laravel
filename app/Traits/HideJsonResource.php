14c51708b5436bf65efebea8bc05be1440b093cf
tarkikRomanski <tarkik@ukr.net>
2018-07-21 15:36:49 +0300

    added trait for hide fields in resource

---
+++ app/Traits/HideJsonResource.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vitaliyprokopov
+ * Date: 7/21/18
+ * Time: 3:30 PM
+ */
+
+namespace App\Traits;
+
+
+trait HideJsonResource
+{
+    /**
+     * @var array
+     */
+    protected static $withoutFields = [];
+
+    /**
+     * Set the keys that are supposed to be filtered out.
+     * @param array $fields
+     * @return $this
+     */
+    public static function hide(array $fields)
+    {
+        self::$withoutFields = $fields;
+        return self::class;
+    }
+
+    /**
+     * Remove the filtered keys.
+     * @param $array
+     * @return array
+     */
+    protected function filterFields($array)
+    {
+        return collect($array)->forget(self::$withoutFields)->toArray();
+    }
+}
