578fead916667ab489808c24bde8c815ee7b66a2
tarkikRomanski <tarkik@ukr.net>
2018-07-22 19:36:08 +0300

    added file type to post table

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,10 +2,18 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\Post::class, function (Faker $faker) {
+$files = [
+    'QDzY3fNd0kwK9jtw0gKye7k81i55xW0BNAjvzcie.pdf',
+    'j1g6CWWgJn2lyUE8qPNB7gbAvZg4620sI8KxahWb.jpeg'
+];
+
+$factory->define(App\Post::class, function (Faker $faker) use ($files) {
+    $file = $faker->randomElement($files);
+    $file_type = \App\Classes\Facades\Uploader::getFileType($file);
     return [
         'name' => $faker->sentence(rand(2, 5)),
         'content' => $faker->text,
-        'file' => $faker->imageUrl($width = 200, $height = 200)
+        'file' => $file,
+        'file_type' => $file_type
     ];
 });
---
+++ database/migrations/2018_07_22_154718_update_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->smallInteger('file_type')
+                ->default(0)
+                ->nullable()
+            ;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('file_type');
+        });
+    }
+}
